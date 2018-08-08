775745dc6fc7e34fab0058058a9f7f44317d4e1d
akrindev <akrinmin@gmail.com>
2018-08-09 00:27:38 +0700

    add fanspage

--- resources/views/inc/tags.blade.php
+++ resources/views/inc/tags.blade.php
@@ -5,6 +5,15 @@ $tags = DB::table('tags')->get();
 $randartikel = DB::table('forums')->inRandomOrder()->take(5)->get();
 
 @endphp
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v3.1&appId=2002599206670692&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <div class="d-none d-sm-block">
 
   @if (count($tags) > 0)
@@ -51,4 +60,8 @@ $i = $color[$rand[0]];
   <br>
 
 @endforeach
+</div>
+
+<div class="my-5">
+<div class="fb-page" data-href="https://www.facebook.com/memetoid/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/memetoid/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/memetoid/">Meme Toram Online Indonesia</a></blockquote></div>
 </div>
