9324964b755ec7c203251bd6ce3bc8a17127f8c1
akrindev <akrinmin@gmail.com>
2018-08-10 01:36:10 +0700

    force to be https if in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
        Schema::defaultStringLength(191);
+		if (env('APP_ENV') === 'production')
+        {
+     		\URL::forceScheme('https');
+        }
     }
 
     /**
