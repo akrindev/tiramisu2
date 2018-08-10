ea6094413f17c4ca73e429d2863b709edae4d2e7
akrindev <akrinmin@gmail.com>
2018-08-10 23:30:34 +0700

    force logout if user got banned

---
+++ app/Http/Middleware/CheckUserPermission.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckUserPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+      	if(auth()->check() &&
+           auth()->user()->banned === 1)
+        {
+          auth()->logout();
+        }
+
+        return $next($request);
+    }
+}
