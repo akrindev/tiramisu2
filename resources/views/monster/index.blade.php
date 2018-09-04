5e601ab6a8622c77881e979bb5fd83b1f9d92b00
akrindev <akrinmin@gmail.com>
2018-09-04 18:27:08 +0700

    into js file

--- resources/views/monster/index.blade.php
+++ resources/views/monster/index.blade.php
@@ -40,25 +40,5 @@
 @endsection
 
 @section('footer')
-<script>
-(function(){
-  let key = document.getElementById('peta-kunci');
-  let peta = document.querySelectorAll('b.kunci');
-
-  key.addEventListener('keyup', (e) => {
-
-    let kunci = key.value.toLowerCase()
-
-    for(let me of peta) {
-      if(me.innerText.toLowerCase().indexOf(kunci) > -1) {
-        me.style.display = "block"
-      } else {
-        me.style.display = "none"
-      }
-    }
-
-  });
-
-})();
-</script>
+<script src="/assets/js/mons.js"></script>
 @endsection
