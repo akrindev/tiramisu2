70d5d4401558b7fd44ffc8a35ae0e1dc9db818a0
akrindev <akrinmin@gmail.com>
2018-09-09 23:59:23 +0700

    search form deskripsi

--- resources/views/inc/cari.blade.php
+++ resources/views/inc/cari.blade.php
@@ -7,6 +7,7 @@
                   </span>
                   <input type="search" name="q" class="form-control" placeholder="Nama item, weapon, armor, monster dll . . ." value="{{ request()->q ?? '' }}" pattern=".{2,}" title="2 karakter atau lebih" required>
                 </div>
+    <small class="text-muted">cari dalam bahasa indonesia</small>
    </div>
   </div>
 {!! form_close() !!}
