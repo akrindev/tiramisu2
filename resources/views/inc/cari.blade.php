e004b762a95f266252e7f5446f5bef8d3a590e29
akrindev <akrinmin@gmail.com>
2018-09-09 23:56:27 +0700

    fix form search

--- resources/views/inc/cari.blade.php
+++ resources/views/inc/cari.blade.php
@@ -1,15 +1,12 @@
 {!! form_open('/search',["method"=>"GET"]) !!}
 <div class="form-group">
-  <div class="input-group">
+  <div>
                 <div class="input-icon">
                   <span class="input-icon-addon">
                     <i class="fe fe-search"></i>
                   </span>
-                  <input type="search" name="q" class="form-control" placeholder="Search here . . ." value="{{ request()->q ?? '' }}" pattern=".{2,}" title="2 karakter atau lebih" required>
+                  <input type="search" name="q" class="form-control" placeholder="Nama item, weapon, armor, monster dll . . ." value="{{ request()->q ?? '' }}" pattern=".{2,}" title="2 karakter atau lebih" required>
                 </div>
-    <span class="input-group-append">
-                    <button class="btn btn-outline-primary" type="submit">Search! </button>
-                   </span>
    </div>
   </div>
 {!! form_close() !!}
