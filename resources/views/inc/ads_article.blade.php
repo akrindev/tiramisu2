5970ea805cf57db689408a2da77e2a46f6514033
akrindev <akrinmin@gmail.com>
2019-01-07 21:09:29 +0700

    add margin

--- resources/views/inc/ads_article.blade.php
+++ resources/views/inc/ads_article.blade.php
@@ -1,1 +1,13 @@
-<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-3650356173552443" data-ad-slot="6407452568"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+<div class="mb-5">
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-3650356173552443"
+     data-ad-slot="6407452568">
+  </ins>
+
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
+</div>
