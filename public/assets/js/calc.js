9de02c0e9eac875eddea1b3b1e4f1dbf2b854cff
akrindev <akrinmin@gmail.com>
2019-01-10 22:22:06 +0700

    impoved exp calculator

--- public/assets/js/calc.js
+++ public/assets/js/calc.js
@@ -15,18 +15,18 @@ if(Number(lvnow) > Number(target_level)) {
 
   var xp = ((Number(lvnow)**4)/40)*(1-Number(lvperc)/100)+(((Number(target_level)-1)*(2*(Number(target_level)-1)+1)*Number(target_level)*(3*(Number(target_level)-1)**2+3*(Number(target_level)-1)-1))-Number(lvnow)*(2*Number(lvnow)+1)*(Number(lvnow)+1)*(3*Number(lvnow)**2+3*Number(lvnow)-1))/1200+((2*Number(lvnow))*(1-Number(lvperc)/100))+((Number(target_level)-1)*Number(target_level)-Number(lvnow)*(Number(lvnow)+1));
 
-  var last_nc = (((Number(lvnow)**4)/40)*(1-Number(lvperc)/100)+(((Number(target_level)-1)*(2*(Number(target_level)-1)+1)*Number(target_level)*(3*(Number(target_level)-1)**2+3*(Number(target_level)-1)-1))-Number(lvnow)*(2*Number(lvnow)+1)*(Number(lvnow)+1)*(3*Number(lvnow)**2+3*Number(lvnow)-1))/1200+((2*Number(lvnow))*(1-Number(lvperc)/100))+((Number(target_level)-1)*Number(target_level)-Number(lvnow)*(Number(lvnow)+1)))/300000;
+  var last_nc = Math.ceil(xp/300000);
 
-  var last_bnut = (((Number(lvnow)**4)/40)*(1-Number(lvperc)/100)+(((Number(target_level)-1)*(2*(Number(target_level)-1)+1)*Number(target_level)*(3*(Number(target_level)-1)**2+3*(Number(target_level)-1)-1))-Number(lvnow)*(2*Number(lvnow)+1)*(Number(lvnow)+1)*(3*Number(lvnow)**2+3*Number(lvnow)-1))/1200+((2*Number(lvnow))*(1-Number(lvperc)/100))+((Number(target_level)-1)*Number(target_level)-Number(lvnow)*(Number(lvnow)+1)))/18270;
+  var last_bnut = Math.ceil(xp/18270);
 
-  var last_nwood = (((Number(lvnow)**4)/40)*(1-Number(lvperc)/100)+(((Number(target_level)-1)*(2*(Number(target_level)-1)+1)*Number(target_level)*(3*(Number(target_level)-1)**2+3*(Number(target_level)-1)-1))-Number(lvnow)*(2*Number(lvnow)+1)*(Number(lvnow)+1)*(3*Number(lvnow)**2+3*Number(lvnow)-1))/1200+((2*Number(lvnow))*(1-Number(lvperc)/100))+((Number(target_level)-1)*Number(target_level)-Number(lvnow)*(Number(lvnow)+1)))/10080;
+  var last_nwood = Math.ceil(xp/10080);
 
   var xp_needed = cm(Math.round(xp));
 
   $(".last_xp").text(xp_needed).css({color:'green'});
-  $(".last_nc").text(lvnow < 50 ? 'min lv 50' : last_nc.toFixed(1)).css({color:'red'});
-  $(".last_bnut").text(lvnow < 25 ? 'min lv 25' : last_bnut.toFixed(1)).css({color:'red'});
-  $(".last_nwood").text(lvnow < 27 ? 'min lv 27' : last_nwood.toFixed(1)).css({color:'red'});
+  $(".last_nc").text(lvnow < 50 ? 'min lv 50' : last_nc.toFixed(0)).css({color:'red'});
+  $(".last_bnut").text(lvnow < 25 ? 'min lv 25' : last_bnut.toFixed(0)).css({color:'red'});
+  $(".last_nwood").text(lvnow < 27 ? 'min lv 27' : last_nwood.toFixed(0)).css({color:'red'});
 
   var spina_nc = last_nc * nc;
   $(".spina_last_nc").text(lvnow < 50 ? 'min lv 50' : cm(Math.round(spina_nc))+'s').css({color:'blue'});
