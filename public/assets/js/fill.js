26ed13efed67655b13ebf4dca837c05c15614e8a
akrindev <akrinmin@gmail.com>
2019-01-10 11:43:08 +0700

    for some reason

--- public/assets/js/fill.js
+++ public/assets/js/fill.js
@@ -205,7 +205,7 @@ class FORMULA {
         let potential = ((data.value > value ? data.value : value) - (data.value > value ? value : data.value)) * stat.pot;
         if (stat.type !== this.weap_arm && ['a', 'w'].includes(stat.type)) potential *= 2;
         if(['e'].includes(stat.type)) potential /= 10;
-        if (increment < 0) potential *= 0.305;
+        if (increment < 0) potential *= 0.3;
         else potential *= -1;
         // ... and determine penalty
         potential *= this.determinePenalty();
