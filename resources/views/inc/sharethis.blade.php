79628e94a474810e7b8987eb0e33aab1447a532a
akrindev <akrinmin@gmail.com>
2019-03-27 09:12:33 +0700

    f#

--- resources/views/inc/sharethis.blade.php
+++ resources/views/inc/sharethis.blade.php
@@ -1,3 +1,3 @@
 <!—- ShareThis BEGIN -—>
-<script async src="//platform-api.sharethis.com/js/sharethis.js#property=5c9ad7ac9852eb0011ce9d6f&product=sticky-share-buttons"></script>
+<script type='text/javascript' src='//platform-api.sharethis.com/js/sharethis.js#property=5c9ad7ac9852eb0011ce9d6f&product=sticky-share-buttons' async='async'></script>
 <!—- ShareThis END -—>
