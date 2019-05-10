753a1f16ca2f1367dff19692b01612d227a6658e
akrindev <akrinmin@gmail.com>
2019-05-11 05:04:40 +0700

    fix details

--- resources/views/monster/items.blade.php
+++ resources/views/monster/items.blade.php
@@ -52,7 +52,7 @@
                 @forelse($item->monsters as $monster)
                 <i class="fe fe-github mr-2"></i><a href="/monster/{{ $monster->id }}" class="mr-1">{{ $monster->name }}</a> <small><a class="text-muted" href="/peta/{{ $monster->map->id }}"> [{{ $monster->map->name }}]</a></small> <br >
                 @empty
-                  -- Tidak di ketahui --
+                  <i class="fe fe-eye mr-2"></i><a href="/item/{{ $item->id }}">Lihat... </a>
                 @endforelse
               </div>
             </details>
