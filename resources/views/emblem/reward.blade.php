1cf3c7b47153ff96f99e547f9ad8a763eefbf4c0
akrindev <akrinmin@gmail.com>
2019-05-29 13:15:48 +0700

    fix

--- resources/views/emblem/reward.blade.php
+++ resources/views/emblem/reward.blade.php
@@ -50,7 +50,7 @@ Cara melihat prestasi. <br> "Menu" &gt; "Karakter" &gt; "Prestasi"
           <div class="mb-5">
           <b>Tampilkan reward: </b> <a href="/prestasi/reward/orb">Orb</a>, <a href="/prestasi/reward/karcis kostum">Karcis Kostum</a>, <a href="/prestasi/reward/stat point">Stat Point</a>, <a href="/prestasi/reward/skill point">Skill Point</a>, <a href="/prestasi/reward/exp gain">EXP Gain</a>, <a href="/prestasi/reward/spina">Spina</a>
           </div>
-          @foreach((new App\Emblem)->get() as $emb)
+          @foreach((new App\Emblem)->orderBy('id')->get() as $emb)
           <div class="mb-1">
           <img src="/img/prestasi.png" class="avatar avatar-sm"> <a href="/prestasi/{{ $emb->id }}" class="ml-2">{{ $emb->name }}</a>
           </div>
