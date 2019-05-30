0c9bdb3297182a5e0d3b90170b9984fce2402122
akrindev <akrinmin@gmail.com>
2019-05-30 07:30:57 +0700

    add scroll to top

--- resources/views/fillstats/home.blade.php
+++ resources/views/fillstats/home.blade.php
@@ -91,7 +91,25 @@
 
   </div>
 </div>
+<button class="btn btn-md btn-primary" onclick="topFunction()" id="myBtn" title="Go to top"><i class="fe fe-arrow-up mr-2"></i>Ke Atas</button>
+<style>
+  #myBtn {
+  display: none; /* Hidden by default */
+  position: fixed; /* Fixed/sticky position */
+  bottom: 70px; /* Place the button at the bottom of the page */
+  right: 30px; /* Place the button 30px from the right */
+  z-index: 99; /* Make sure it does not overlap */
+ /* border: none; /* Remove borders */
+  /*outline: none; /* Remove outline */
+ /* background-color: red; /* Set a background color */
+ /* color: white; /* Text color */
+  cursor: pointer; /* Add a mouse pointer on hover */
+ /* padding: 15px; /* Some padding */
+/*  border-radius: 10px; /* Rounded corners */
+ /* font-size: 18px; /* Increase font size */
+}
 
+</style>
 @endsection
 
 @section('head')
@@ -99,5 +117,21 @@
   $('#filler').ready(function() {
  	$("#filload").remove();
   });
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 </script>
 @endsection
