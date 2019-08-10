6368fca2fae42ef3ca8e9add13f8df2ddc1c9715
akrindev <akrinmin@gmail.com>
2019-08-10 07:10:12 +0700

    added exp quest calculator

--- public/assets/js/calc.js
+++ public/assets/js/calc.js
@@ -1,52 +1,87 @@
-function calc()
-{
 
-  var nc = $("#nc").val();
-  var bnut = $("#bnut").val();
-  var nwood = $("#nwood").val();
+const app = new Vue({
+  el: "#vue-root",
+  data() {
+    return {
+      hnc: 16000,
+      hbnut: 5000,
+      hnwood: 4000,
+      lvnow: 25,
+      lvperc: 0,
+      lvtarget: 190,
 
-  var lvnow = $("#lvnow").val();
-  var lvperc = $("#lvperc").val();
-  var target_level = $("#lvtarget").val();
+      // required exp
+    //  lastExp: 0,
+      lastNc: '',
+      lastBnut: '',
+      lastNwood: '',
 
-if(Number(lvnow) > Number(target_level)) {
-  alert('level now greater than target level!!')
-}
+      // required spina
+      spinaNc: '',
+      spinaBnut: '',
+      spinaNwood: '',
 
-  var xp = ((Number(lvnow)**4)/40)*(1-Number(lvperc)/100)+(((Number(target_level)-1)*(2*(Number(target_level)-1)+1)*Number(target_level)*(3*(Number(target_level)-1)**2+3*(Number(target_level)-1)-1))-Number(lvnow)*(2*Number(lvnow)+1)*(Number(lvnow)+1)*(3*Number(lvnow)**2+3*Number(lvnow)-1))/1200+((2*Number(lvnow))*(1-Number(lvperc)/100))+((Number(target_level)-1)*Number(target_level)-Number(lvnow)*(Number(lvnow)+1));
+      // required quest
+      // quest arwah peneliti
+      q1: '',
+      q2: '',
+      q3: '',
+      q4: '',
+      q5: '',
+      q6: '',
+      q7: '',
+      q8: '',
 
-  var last_nc = Math.ceil(xp/300000);
-
-  var last_bnut = Math.ceil(xp/18270);
-
-  var last_nwood = Math.ceil(xp/10080);
-
-  var xp_needed = cm(Math.round(xp));
-
-  $(".last_xp").text(xp_needed + ' exp').css({color:'green'});
-  $(".from_lvl").text(lvnow + ' - ' + target_level);
-  $(".last_nc").text(lvnow < 50 ? 'min lv 50' : last_nc.toFixed(0)).css({color:'red'});
-  $(".last_bnut").text(lvnow < 25 ? 'min lv 25' : last_bnut.toFixed(0)).css({color:'red'});
-  $(".last_nwood").text(lvnow < 27 ? 'min lv 27' : last_nwood.toFixed(0)).css({color:'red'});
-
-  var spina_nc = last_nc * nc;
-  $(".spina_last_nc").text(lvnow < 50 ? 'min lv 50' : cm(Math.round(spina_nc))+'s').css({color:'blue'});
-
-  var spina_bnut = last_bnut * bnut;
-  $(".spina_last_bnut").text(lvnow < 25 ? 'min lv 25' : cm(Math.round(spina_bnut))+'s').css({color:'blue'});
-
-  var spina_nwood = last_nwood * nwood;
-  $(".spina_last_nwood").text(lvnow < 27 ? 'min lv 27' : cm(Math.round(spina_nwood))+"s").css({color:'blue'});
-}
-
-function cm(input) {
-    var output = input
-    if (parseFloat(input)) {
-        input = new String(input); // so you can perform string operations
-        var parts = input.split("."); // remove the decimal part
-        parts[0] = parts[0].split("").reverse().join("").replace(/(\d{3})(?!$)/g, "$1,").split("").reverse().join("");
-        output = parts.join(".");
     }
+  },
+  computed: {
+    lastExp: function() {
+      var exp = ((Number(this.lvnow)**4)/40)*(1-Number(this.lvperc)/100)+(((Number(this.lvtarget)-1)*(2*(Number(this.lvtarget)-1)+1)*Number(this.lvtarget)*(3*(Number(this.lvtarget)-1)**2+3*(Number(this.lvtarget)-1)-1))-Number(this.lvnow)*(2*Number(this.lvnow)+1)*(Number(this.lvnow)+1)*(3*Number(this.lvnow)**2+3*Number(this.lvnow)-1))/1200+((2*Number(this.lvnow))*(1-Number(this.lvperc)/100))+((Number(this.lvtarget)-1)*Number(this.lvtarget)-Number(this.lvnow)*(Number(this.lvnow)+1));
 
-    return output;
-}
+      return Math.trunc(exp)
+    }
+  },
+  methods: {
+    kalkulasi() {
+
+      if(this.lvnow > this.lvtarget) {
+        alert('Nilai Level sekarang > level target')
+        return;
+      }
+
+      // stack
+      this.lastNc = this.lvnow < 50 ? 'min lv 50' :  Math.ceil(this.lastExp/300000)
+      this.lastBnut = this.lvnow < 25 ? 'min lv 25' : Math.ceil(this.lastExp/18270)
+      this.lastNwood = this.lvnow < 27 ? 'min lv 27' :  Math.ceil(this.lastExp/10080)
+
+      // total spina
+      this.spinaNc = this.lvnow < 50 ? 'min lv 50' : Math.round(this.lastNc*this.hnc)
+      this.spinaBnut = this.lvnow < 25 ? 'min lv 25' : Math.round(this.lastBnut*this.hbnut)
+      this.spinaNwood = this.lvnow < 27 ? 'min lv 27' : Math.round(this.lastNwood*this.hnwood)
+
+
+      // QUEST
+      this.q1 = this.lvnow < 89 ? 'Min lv 89' : Math.round(this.lastExp/150000)
+      this.q2 = this.lvnow < 78 ? 'Min lv 78' : Math.round(this.lastExp/55200)
+      this.q3 = this.lvnow < 86 ? 'Min lv 86' : Math.round(this.lastExp/50600)
+      this.q4 = this.lvnow < 84 ? 'Min lv 84' : Math.round(this.lastExp/62800)
+      this.q5 = this.lvnow < 82 ? 'Min lv 82' : Math.round(this.lastExp/41800)
+      this.q6 = this.lvnow < 83 ? 'Min lv 83' : Math.round(this.lastExp/344000)
+      this.q7 = this.lvnow < 81 ? 'Min lv 81' : Math.round(this.lastExp/175000)
+      this.q8 = this.lvnow < 77 ? 'Min lv 77' : Math.round(this.lastExp/133000)
+    }
+  },
+  filters: {
+    numberFormat(input) {
+      var output = input
+      if (parseFloat(input)) {
+          input = new String(input); // so you can perform string operations
+          var parts = input.split("."); // remove the decimal part)
+          parts[0] = parts[0].split("").reverse().join("").replace(/(\d{3})(?!$)/g, "$1,").split("").reverse().join("");
+          output = parts.join(".");
+      }
+
+      return output;
+    }
+  }
+});
--- resources/views/xp_calculator.blade.php
+++ resources/views/xp_calculator.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.tabler')
 
-@section('title','Exp Calculator')
-@section('description','Toram exp calculator, Nc, bnut, dan lainnya. kuy cek disini')
+@section('title','Exp Calculator including Quest')
+@section('description','Kalkulasi jumlah exp yang di butuhkan dari Nc, bnut sampai Quest. kuy cek disini')
 @section('image',to_img())
 
 @push('canonical')
@@ -9,41 +9,48 @@
 @endpush
 
 @section('content')
-        <div class="my-3 my-md-5">
-          <div class="container">
+  <div class="my-3 my-md-5">
+    <div class="container">
+
+    <div class="page-header">
+      <h1 class="page-title"> Exp Calculator including Quest</h1>
+    </div>
+
+
+  @includeWhen(env('APP_ENV') == 'production', 'inc.ads_mobile')
+
    @include('inc.cari')
-              <div class="card">
+              <div class="card" id="vue-root">
                   <div class="card-header">
-                  <h3 class="card-title">Toram Exp calculator</h3>
+                  <h3 class="card-title">Toram Exp calculator termasuk quest</h3>
                   </div>
                   <div class="card-alert alert alert-info">
                     <b>New!!</b> Fill stats calculator <a href="/fill_stats/calculator"> Click here </a><br>
                     <b>Hot!!</b> Have you try? <a href="/leveling">Toram Leveling Finder</a>
                   </div>
 
-  @includeWhen(env('APP_ENV') == 'production', 'inc.ads_mobile')
-                <div class="card-body">
+  <div class="card-body p-3">
   <div class="row">
 
     <div class="h3 mb-5 col-12">Price (harga)</div>
 
     <div class="form-group form-group-sm col-md-4 col-xs-12">
       <label>Nightmare Cystal (Kristal teror)</label>
-      <input type="number" id="nc" class="form-control" value="10000">
+      <input type="number" id="nc" class="form-control" v-model="hnc">
       <div class="help-block">Harga NC per stk</div>
     </div>
 
 
     <div class="form-group form-group-sm col-md-4 col-xs-12">
       <label>Bitter Nut (Kacang Pahit)</label>
-      <input type="number" id="bnut" class="form-control" value=5000>
+      <input type="number" id="bnut" class="form-control" v-model="hbnut">
       <div class="help-block">Harga BNut perstk </div>
     </div>
 
 
     <div class="form-group form-group-sm col-md-4 col-xs-12">
       <label>Nisel Wood (Kayu Nisel)</label>
-      <input type="number" id="nwood" class="form-control" value="2000">
+      <input type="number" id="nwood" class="form-control" v-model="hnwood">
       <div class="help-block">Harga NWood perstk</div>
     </div>
 
@@ -55,44 +62,70 @@
     <div class="h2 mb-5 col-12">Level</div>
     <div class="form-group form-group-sm col-md-4 col-xs-6">
       <label>Level Sekarang</label>
-      <input type="number" id="lvnow" class="form-control" value="25">
+      <input type="number" id="lvnow" class="form-control" v-model="lvnow">
       <div class="help-block"></div>
     </div>
 
     <div class="form-group form-group-sm col-md-4 col-xs-6">
       <label>Level Persen sekarang (%) <small class="text-muted">(0-100%)</small> </label>
-      <input type="number" id="lvperc" class="form-control" value="0">
+      <input type="number" id="lvperc" class="form-control" v-model="lvperc">
       <div class="help-block"></div>
     </div>
 
     <div class="form-group form-group-sm col-md-4 col-xs-6">
       <label>Target Level</label>
-      <input type="number" id="lvtarget" class="form-control" value="190">
+      <input type="number" id="lvtarget" class="form-control" v-model="lvtarget">
       <div class="help-block"></div>
     </div>
 
-    <div class="col-md-4 mb-6 col-xs-6">
+    <div class="col-md-12 mb-6 col-xs-6">
       <label>Mulai hitung ? </label><br>
-      <button onClick="calc()" class="btn btn-primary">Kalkulasi</button>
+      <button @click="kalkulasi" class="btn btn-primary">Kalkulasi</button>
     </div>
 
-	<div class="col-md-12 col-xs-12">
+	<div class="col-md-6 col-xs-12">
     <h2>Hasil Akhir</h2>
-  <b>Leveling:</b> <span class="from_lvl"></span><br><br>
-    <b>Exp yang di butuhkan:</b> <span class="last_xp"></span><br /><br />
+  <b>Leveling:</b> <span class="text-primary">@{{ lvnow }} - @{{ lvtarget }}</span> <br>
+    <b>Exp yang di butuhkan:</b> <span class="text-success">@{{ lastExp | numberFormat }}</span><br /><br />
     <b>Stack yang di butuhkan:</b>
-    <div class="container-fluid">
-      <b>NC: </b>  <span class="last_nc"></span><br />
-         <b>Bnut: </b>  <span class="last_bnut"></span><br />
-         <b>Nwood: </b>  <span class="last_nwood"></span><br /><br /><br />
-    </div>
+    <ol>
+      <li><b>NC: </b>  <span class="text-danger">@{{ lastNc }}</span></li>
+      <li><b>Bnut: </b>  <span class="text-danger">@{{ lastBnut }}</span></li>
+      <li><b>Nwood: </b>  <span class="text-danger">@{{ lastNwood }}</span></li>
+    </ol>
 
     <b>Spina yang di butuhkan:</b>
-    <div class="container-fluid">
-      <b>NC: </b>  <span class="spina_last_nc"></span><br />
-         <b>Bnut: </b>  <span class="spina_last_bnut"></span><br />
-         <b>Nwood: </b>  <span class="spina_last_nwood"></span>
+    <ol>
+      <li><b>NC: </b>  <span class="text-primary">@{{ spinaNc | numberFormat }}</span></li>
+      <li><b>Bnut: </b>  <span class="text-primary">@{{ spinaBnut | numberFormat }}</span></li>
+      <li><b>Nwood: </b>  <span class="text-primary">@{{ spinaNwood | numberFormat }}</span></li>
+    </ol>
     </div>
+
+    <div class="col-md-6 col-xs-12">
+      <h3> Hasil Kalkulasi Quest </h3>
+
+      <h6> Quest NPC: Arwah Peneliti </h6>
+      <ol>
+        <li>
+      <strong><a href="/npc/quest/1">Bebas Dari si Penggerogot!</a> </strong> <span class="text-danger">@{{ q1 }}</span></li>
+        <li><strong><a href="/npc/quest/2">Mencari Air Bersih</a></strong> <span class="text-danger">@{{ q2 }}</span></li>
+        <li><strong> <a href="/npc/quest/3">Buku Mestinya di Rak Buku</a> </strong> <span class="text-danger">@{{ q3 }}</span></li>
+        <li><strong> <a href="/npc/quest/4">Secercah Harapan</a> </strong> <span class="text-danger">@{{ q4 }}</span></li>
+           <li><strong> <a href="/npc/quest/5">Pengejaran si Biang Kerok!</a> </strong> <span class="text-danger">@{{ q5 }}</span></li>
+      </ol>
+
+      <hr class="my-2">
+
+
+      <h6> Quest NPC: Arwah Ningrat </h6>
+      <ol>
+        <li>
+      <strong><a href="/npc/quest/6">Kelinci di Alam Kegelapan?!</a> </strong> <span class="text-danger">@{{ q6 }}</span></li>
+        <li><strong><a href="/npc/quest/7">Hidup Nyaman Terus</a></strong> <span class="text-danger">@{{ q7 }}</span> </li>
+        <li><strong> <a href="/npc/quest/8">Si Aneh Imut</a> </strong> <span class="text-danger">@{{ q8 }}</span></li>
+      </ol>
+
     </div>
 
   @includeWhen(env('APP_ENV') == 'production', 'inc.ads_mobile')
@@ -101,6 +134,13 @@
             </div>
           </div>
 </div>
-<script src="/assets/js/calc.js"></script>
+@endsection
 
+@section('head')
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+@endsection
+
+@section('footer')
+
+<script src="/assets/js/calc.js"></script>
 @endsection
