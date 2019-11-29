9f624188afc7fe112757219647f8681349c635ba
akrindev <akrinmin@gmail.com>
2019-11-29 18:05:00 +0700

    fixed whatsapp number

--- app/Contact.php
+++ app/Contact.php
@@ -9,4 +9,9 @@ class Contact extends Model
   protected $fillable = [
   	'user_id', 'line', 'whatsapp'
   ];
+
+  public function getWhatsappAttribute($value)
+  {
+    return is_null($value) ? null : preg_replace('/(^[0])/', '+62', $value);
+  }
 }
