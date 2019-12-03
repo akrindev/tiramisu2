3219238ca2376fac3f1c4659c7a3e8abec72aea2
akrindev <akrinmin@gmail.com>
2019-12-03 14:27:49 +0700

    fix migration

--- database/migrations/2019_12_03_125838_create_settings_table.php
+++ database/migrations/2019_12_03_125838_create_settings_table.php
@@ -15,7 +15,7 @@ class CreateSettingsTable extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->increments('id');
-          	$table->json('body');
+          	$table->longText('body');
             $table->timestamps();
         });
     }
