be1665b6661e0e718c49ffa6eb3db16c48330082
akrindev <akrinmin@gmail.com>
2020-08-07 05:21:12 +0700

    Updated fill stat

--- resources/views/fillstats/calculator.blade.php
+++ resources/views/fillstats/calculator.blade.php
@@ -31,12 +31,14 @@
         <div class="alert alert-info">
         Untuk melihat fill stats formula <a href="/fill_stats">klik disini</a>
         </div>
-        <div class="alert alert-warning">
-        	Algoritma simulasi ini tidak sepenuhnya efisien, setidaknya koreksi terlebih dahulu barangkali terjadi kesalahan atau bisa juga lihat fill stat formula.
-        </div>
+
       </div>
 
-      <div class="col-md-4">
+    </div>
+
+    <div class="row equal justify-content-center">
+
+      <div class="col-md-6 mb-5">
         <div class="card">
           <div class="card-header">
             <h3 class="card-title"> Resep status </h3>
@@ -44,86 +46,92 @@
 
           <div class="card-body p-3" style="font-size:14px;font-weight:400">
             <div class="form-group">
-               <select class="form-control" id="weap_arm" onchange="update_config('weap_arm')">
-                <option value='w'>Weapon</option>
-                <option value='a'>Zirah</option>
-              </select>
+              <label class="form-label">Type</label>
+              <div class="selectgroup w-100">
+                 <label class="selectgroup-item">
+                     <input onclick="document.querySelector('#recipe_pot').value = 46" type="radio" value="w" class="selectgroup-input" checked="false" name="weap_arm" id="weap_arm">
+                            <span class="selectgroup-button selectgroup-button-icon">Weapon</span>
+                 </label>
+
+                 <label class="selectgroup-item">
+                     <input onclick="document.querySelector('#recipe_pot').value = 44" type="radio" value="a" checked="true" class="selectgroup-input" name="weap_arm" id="weap_arm">
+                            <span class="selectgroup-button selectgroup-button-icon">Armor</span>
+                 </label>
+              </div>
             </div>
 
             <div class="form-group">
-            	<div id="stat_menu" class="row gutter-xs p-2">
-
-            	</div>
+              <label class="form-label">Recipe Pot</label>
+              <input type="number" value="44" class="form-control" id="recipe_pot">
             </div>
+
+
+            <div class="form-group">
+              <label class="form-label">Starting Pot</label>
+                   <input type="number" value="89" class="form-control" id="starting_pot">
+            </div>
+
             <div class="form-group">
               <label class="form-label">Level prof bs</label>
             	<input type="number" min=1 max=200 class="form-control" id="prof" value=1 oninput="update_prof_lv();">
             </div>
 
             <div class="form-group">
-              <button class="btn btn-outline-primary m-1" onclick="get_results()">Lihat!</button>
-              <button class="btn btn-outline-secondary m-1" onclick='prompt_potential()'>Set Potensi</button>
-              <button class="btn btn-outline-success m-1" onclick='saveAsImg()'><i class="fe fe-image"></i> Save as image</button>
-              <button class="btn btn-outline-danger m-1"  onclick="build_menu()"><i class="fe fe-refresh-ccw"></i> Reset</button>
+              <button class="btn btn-outline-primary btn-pill m-1" onclick="start_stat()">Start!</button>
+
               <a href="/fill_stats" class="btn btn-pill btn-outline-warning m-1"><i class="fe fe-settings"></i> Manual formula</a>
 
             </div>
 
 
             <div class="mt-3">
-              <small class="text-muted">Credit: <a href="https://sparkychildcharlie.github.io/statting" rel="nofollow" target="_blank">Charlie Kobayashi</a></small>
+              <small class="text-muted">Credit: <a href="https://sparkychildcharlie.github.io/statting_simulator" rel="nofollow" target="_blank">Charlie Kobayashi</a></small>
             </div>
           </div>
         </div>
       </div>
-
-
-      <div class="col-md-8">
-
+      <div class="w-100"></div>
+      <div class="col-md-5 mb-5 hidden">
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title"> Step </h3>
+            <h3 class="card-title">Status</h3>
           </div>
-
-          <div class=" o-auto" style="font-size:14px;font-weight:400;height: 26rem">
-            <div id="details">
-              Step akan tampil disini
+          <div class="card-body p-3">
+            <div class="form-group">
+              <div class="row gutter-xs p-3" id="workspace"></div>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="col-md-4">
-
+      <div class="col-md-4 mb-5 hidden">
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title"> Hasil </h3>
+            <h3 class="card-title">Formula</h3>
           </div>
-          <div id="hasil-help"></div>
-          <div class="o-auto" style="font-size:14px;font-weight:40p;height: 26rem">
-            <div id="results">
-            Hasil akan tampil disini
-            </div>
+          <div class="card-body p-3" id="formula_display">
           </div>
         </div>
       </div>
 
-      <div class="col-md-8">
-        @includeWhen(!app()->isLocal(), 'inc.ads_mobile')
-      	<div class="card">
+      <div class="col-md-3 mb-5 hidden">
+        <div class="card">
           <div class="card-header">
-            <h3 class="card-title"> Banyak di gunakan </h3>
+            <h3 class="card-title">Material Used</h3>
           </div>
-          <div class="card-body">
-            <div id="stat_formulas"></div>
+          <div class="card-body p-3" id="material_display">
           </div>
         </div>
       </div>
 
-      <div class="col-12">
-        @includeWhen(!app()->isLocal(), 'inc.ads_article')
-      </div>
     </div>
+
+    <!-- ads -->
+    <div class="row">
+      <div class="col-12">
+        @includeWhen(!app()->isLocal(), 'inc.ads_article')</div>
+    </div>
+
   </div>
 </div>
 <!-- Modal -->
@@ -148,14 +156,33 @@
 @endsection
 
 @section('head')
+
+<style>
+  @media (min-width: 768px) {
+  .equal {
+    display: -webkit-box;
+
+  display: -webkit-flex;
+
+  display: -ms-flexbox;
+
+  display:         flex;
+  }
+}
+  .hidden {
+    display: none
+  }
+
+  .card {
+    height: 100%
+  }
+</style>
+@endsection
+
+@section('footer')
+
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="/assets/js/vendors/html2canvas.min.js"></script>
-<script src="/assets/js/fill.js?fill20v1_5"></script>
+<script src="/assets/js/newfill.js"></script>
 
-<script src="/assets/js/formula.js?fill20v1_2"></script>
-<script>
-$("body").ready(function(){
-	build_menu();
-});
-</script>
 @endsection
