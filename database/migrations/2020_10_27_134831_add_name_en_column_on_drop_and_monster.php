6f81a37de67919c12c55a1cc24adb492185504ca
akrindev <akrinmin@gmail.com>
2020-10-27 18:02:07 +0700

    add column name_en to item and monster

---
+++ database/migrations/2020_10_27_134831_add_name_en_column_on_drop_and_monster.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNameEnColumnOnDropAndMonster extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('drops', function (Blueprint $table) {
+        	$table->string('name_en')->after('name')->nullable();
+        });
+
+        Schema::table('monsters', function (Blueprint $table) {
+        	$table->string('name_en')->after('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      if(Schema::hasColumn('drops', 'name_en')) {
+        Schema::table('drops', function (Blueprint $table) {
+        	$table->dropColumn(['name_en']);
+        });
+      }
+
+      if(Schema::hasColumn('monsters', 'name_en')) {
+        Schema::table('monsters', function (Blueprint $table) {
+        	$table->dropColumn(['name_en']);
+        });
+      }
+    }
+}
