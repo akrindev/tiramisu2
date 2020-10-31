3bf7385f766a457dd18f1adeb344dc3c31e74e0d
akrindev <akrinmin@gmail.com>
2020-10-31 12:24:21 +0700

    fix matched ads

--- resources/views/inc/ads_matched.blade.php
+++ resources/views/inc/ads_matched.blade.php
@@ -1,12 +1,10 @@
-<div class="mt-5 container-fluid">
+<div class="my-5 container-fluid">
+    <!-- Matched Pom -->
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- RerenChy -->
 <ins class="adsbygoogle"
-     style="display:block"
+     style="display:inline-block;width:320px;height:950px"
      data-ad-client="ca-pub-3650356173552443"
-     data-ad-slot="9636022497"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
+     data-ad-slot="3012467147"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
