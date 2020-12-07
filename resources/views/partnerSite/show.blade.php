dc89dd1f325d13fa29a345ed667124078d5ad82d
akrindev <akrinmin@gmail.com>
2020-12-07 10:15:03 +0700

    update image partner

--- resources/views/partnerSite/show.blade.php
+++ resources/views/partnerSite/show.blade.php
@@ -4,7 +4,7 @@
 
     <ul class="list-unstyled">
       <li><a href="https://www.youtube.com/channel/UCWjnDhpItCasRJghdyXbpjQ" target="_blank">
-        <img src="/img/ball-triangle.svg" data-src="/partner/vld_miku.png" class="lazyload animated infinite flip" width=150 height="140">
+        <img src="/img/ball-triangle.svg" data-src="/partner/raymiku.png" class="lazyload animated infinite bounce" width=150 height="80">
         </a>
       </li>
     </ul>
