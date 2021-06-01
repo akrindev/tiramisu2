6dcc9ca45429876c0764da9392d822d816e96e5f
akrindev <akrinmin@gmail.com>
2021-06-01 14:58:15 +0700

    guild model

--- app/Guild.php
+++ app/Guild.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Guild extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function users()
+    {
+        return $this->belongToMany(User::class, 'user_guild');
+    }
+
+    public function manager()
+    {
+        return $this->belongsTo(User::class, 'manager_id');
+    }
 }
