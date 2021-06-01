58b9693d8eeff8a705d3ca3a960404fae9b0e1c3
akrindev <akrinmin@gmail.com>
2021-06-01 14:50:14 +0700

    prepare guild migration

---
+++ database/migrations/2021_06_01_143657_create_guilds_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuildsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guilds', function (Blueprint $table) {
+            $table->id();
+            $table->integer('manager_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('logo');
+            $table->timestamps();
+        });
+
+        Schema::create('user_guild', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('guild_id');
+            $table->enum('role', ['ketua', 'wakil', 'inviter']);
+            $table->integer('manager_id')->nullable();
+            $table->tinyInteger('accept')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guilds');
+        Schema::dropIfExists('user_guild');
+    }
+}
