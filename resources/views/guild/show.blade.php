836f061b1c80634c7c15adc2b80a5918ec541cf5
akrindev <akrinmin@gmail.com>
2021-06-14 10:09:10 +0700

    fixed

--- resources/views/guild/show.blade.php
+++ resources/views/guild/show.blade.php
@@ -160,10 +160,10 @@
                                             @if ($member->cooking)
 
                                             <span class="text-muted">
-                                                {{ (new \App\Helpers\Food)->getStatLv($member->cooking->buff, $member->cooking->stat, $member->cooking_level, true) ?? '' }}
+                                                {{ (new \App\Helpers\Food)->getStatLv(optional($member->cooking)->buff, $member->cooking->stat, $member->cooking_level, true) ?? '' }}
                                             </span> <br>
                                             <span class="text-muted">
-                                                 {{ (new \App\Helpers\Food)->getStatLv($member->secondCooking->buff, $member->secondCooking->stat, $member->second_cooking_level, true) ?? ''}}
+                                                 {{ (new \App\Helpers\Food)->getStatLv(optional($member->secondCooking)->buff, $member->secondCooking->stat, $member->second_cooking_level, true) ?? ''}}
                                             </span>
                                             @endif
                                         </div>
