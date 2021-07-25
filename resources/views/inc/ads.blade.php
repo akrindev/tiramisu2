a4ee10ac8a7e63eecd80c71e5fbe9bfd72d0980e
akrindev <akrinmin@gmail.com>
2021-07-25 11:05:16 +0700

    update ads

--- resources/views/inc/ads.blade.php
+++ resources/views/inc/ads.blade.php
@@ -1,10 +1,5 @@
 @if (!(new \App\User)->isTopContributor())
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-3650356173552443",
-    enable_page_level_ads: true
-  });
-</script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3650356173552443"
+     crossorigin="anonymous"></script>
 @endif
--- resources/views/inc/ads_mobile.blade.php
+++ resources/views/inc/ads_mobile.blade.php
@@ -1,12 +1,16 @@
 @if (!(new \App\User)->isTopContributor())
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Mobile version -->
+<div class="ads">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3650356173552443"
+     crossorigin="anonymous"></script>
+<!-- new mobile -->
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-3650356173552443"
-     data-ad-slot="8584995088"
-     data-ad-format="auto"></ins>
+     data-ad-slot="8988721398"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
+</div>
 @endif
