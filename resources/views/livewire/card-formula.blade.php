47e0581484030513676b1fd91e715caaa42b7f8b
akrindev <akrinmin@gmail.com>
2022-04-30 05:26:54 +0700

    clean up

--- resources/views/livewire/card-formula.blade.php
+++ resources/views/livewire/card-formula.blade.php
@@ -1,59 +1,58 @@
+<div class="col-md-4">
+    <div class="card">
+        <div class="card-header p-2">
+            <h3 class="card-title">{{ $formula->note }} </h3>
+        </div>
 
-    <div class="col-md-4">
-            <div class="card">
-                <div class="card-header p-2">
-                    <h3 class="card-title">{{ $formula->note }} </h3>
-                </div>
+        @if(session()->has('fail'))
+        <div class="card-alert alert alert-danger"> {{ session('fail') }} </div>
+        @endif
 
-				@if(session()->has('fail'))
-				<div class="card-alert alert alert-danger"> {{ session('fail') }} </div>
-				@endif
+        <div class="p-2 card-body">
+            <div class="mb-2 d-block">
 
-                <div class="p-2 card-body">
-                    <div class="mb-2 d-block">
+                <table width="100%">
+                    <tr>
+                        <th width="35%"> Type</th>
+                        <td class=""> {{ $formula->type }} </td>
+                    </tr>
+                    <tr>
+                        <th width="35%"> Starting Pot </th>
+                        <td> {{ $formula->starting_pot }} </td>
+                    </tr>
+                    <tr>
+                        <th width="35%"> Highest Mats </th>
+                        <td> {{ $formula->highest_mats }} </td>
+                    </tr>
+                    <tr class="{{ $formula->success_rate < 100 ? 'text-danger' : 'text-success'}}">
+                        <th width="35%"> Success Rate </th>
+                        <td> {{ $formula->success_rate }}%</td>
+                    </tr>
+                </table>
 
-                        <table width="100%">
-                            <tr>
-                                <th width="35%"> Type</th>
-                                <td class=""> {{ $formula->type }} </td>
-                            </tr>
-                            <tr>
-                                <th width="35%"> Starting Pot </th>
-                                <td> {{ $formula->starting_pot }} </td>
-                            </tr>
-                            <tr>
-                                <th width="35%"> Highest Mats </th>
-                                <td> {{ $formula->highest_mats }} </td>
-                            </tr>
-                            <tr class="{{ $formula->success_rate < 100 ? 'text-danger' : 'text-success'}}">
-                                <th width="35%"> Success Rate </th>
-                                <td> {{ $formula->success_rate }}%</td>
-                            </tr>
-                        </table>
+            </div>
 
-                    </div>
+            <div class="px-3 py-2 bg-blue-lightest">
+            {!! $formula->final_step !!}
+            </div>
 
-                    <div class="px-3 py-2 bg-blue-lightest">
-                    {!! $formula->final_step !!}
-                    </div>
+            <div class="mt-2">
+                <small class="float-left text-muted"><b>Created: </b> {{ $formula->created_at->format('d-M-Y H:i') }} </small>
 
-                    <div class="mt-2">
-                        <small class="float-left text-muted"><b>Created: </b> {{ $formula->created_at->format('d-M-Y H:i') }} </small>
+                <button
+                    @if (auth()->check() && in_array(auth()->id(), $formula->users->pluck('id')->toArray()))
+                        class="float-right btn btn-sm btn-pill btn-danger disabled" disabled
+                    @else
+                        wire:click="save({{ $formula->id }})"
+                        wire:loading.class="btn-loading"
+                        class="float-right btn btn-sm btn-pill btn-danger"
+                    @endif >
+                    {{ $formula->users->count() > 0 ? $formula->users->count() : '' }}
+                    {{ Illuminate\Support\Str::plural('love', $formula->users->count()) }}
+                </button>
 
-                        <button
-                            @if (auth()->check() && in_array(auth()->id(), $formula->users->pluck('id')->toArray()))
-                                class="float-right btn btn-sm btn-pill btn-danger disabled" disabled
-                            @else
-                                wire:click="save({{ $formula->id }})"
-                                wire:loading.class="btn-loading"
-                                class="float-right btn btn-sm btn-pill btn-danger"
-                            @endif >
-                            {{ $formula->users->count() > 0 ? $formula->users->count() : '' }}
-                            {{ Illuminate\Support\Str::plural('love', $formula->users->count()) }}
-                        </button>
-
-                        <a wire:click.prevent="show({{ $formula->id }})" wire:loading.class="btn-loading bg-primary" href="/fill_stats/show/{{ $formula->id }}" class="float-right mx-1 btn btn-sm btn-pill btn-outline-primary">Show</a>
-                    </div>
-                </div>
+                <a wire:click.prevent="show({{ $formula->id }})" wire:loading.class="btn-loading bg-primary" href="/fill_stats/show/{{ $formula->id }}" class="float-right mx-1 btn btn-sm btn-pill btn-outline-primary">Show</a>
             </div>
         </div>
+    </div>
+</div>
--- resources/views/livewire/formula.blade.php
+++ resources/views/livewire/formula.blade.php
@@ -1,50 +1,47 @@
 <div>
     <h4 class="text-center text-muted">Found {{ $formulas->total() }} Results</h4>
-<div class="row">
-    <div class="col-md-9">
-        <div class="card">
-            <div class="p-3 card-body">
+    <div class="row">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="p-3 card-body">
 
-            <div class="selectgroup w-100">
-                <label class="selectgroup-item">
-                    <input type="radio" name="value" value="id" class="selectgroup-input" checked="" wire:click="formulaType('all')">
-                    <span class="selectgroup-button">All</span>
-                </label>
-                <label class="selectgroup-item">
-                    <input type="radio" name="value" value="id" class="selectgroup-input" wire:click="formulaType('a')">
-                    <span class="selectgroup-button">Armor</span>
-                </label>
-                <label class="selectgroup-item">
-                    <input type="radio" name="value" value="w" class="selectgroup-input" wire:click="formulaType('w')">
-                    <span class="selectgroup-button">Weapon</span>
-                </label>
-            </div>
+                <div class="selectgroup w-100">
+                    <label class="selectgroup-item">
+                        <input type="radio" name="value" value="id" class="selectgroup-input" checked="" wire:click="formulaType('all')">
+                        <span class="selectgroup-button">All</span>
+                    </label>
+                    <label class="selectgroup-item">
+                        <input type="radio" name="value" value="id" class="selectgroup-input" wire:click="formulaType('a')">
+                        <span class="selectgroup-button">Armor</span>
+                    </label>
+                    <label class="selectgroup-item">
+                        <input type="radio" name="value" value="w" class="selectgroup-input" wire:click="formulaType('w')">
+                        <span class="selectgroup-button">Weapon</span>
+                    </label>
+                </div>
 
-			<div class="form-group">
-				<label class="form-label"> Search </label>
-				<input type="search" wire:model.debounce.750ms="search" placeholder="title to search" class="form-control"/>
-			</div>
+                <div class="form-group">
+                    <label class="form-label"> Search </label>
+                    <input type="search" wire:model.debounce.750ms="search" placeholder="title to search" class="form-control"/>
+                </div>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div wire:loading class="col-md-3">
-        <div class="alert alert-success">Loading . . .</div>
-    </div>
+        <div wire:loading class="col-md-3">
+            <div class="alert alert-success">Loading . . .</div>
+        </div>
 
-    @foreach ($formulas as $formula)
-        @if(($loop->index + 1) % 5 == 0)
-            @includeUnless(app()->isLocal(), 'inc.ads_article')
-        @endif
-        @livewire('card-formula', ['formula' => $formula], key($formula->id))
-    @endforeach
+        @foreach ($formulas as $formula)
+            @livewire('card-formula', ['formula' => $formula], key($formula->id))
+        @endforeach
 
-    <div wire:loading class="col-md-3">
-        <div class="alert alert-success">Loading . . .</div>
-    </div>
+        <div wire:loading class="col-md-3">
+            <div class="alert alert-success">Loading . . .</div>
+        </div>
 
-    <div class="col-md-12">
-    	{{ $formulas->onEachSide(1)->links() }}
+        <div class="col-md-12">
+            {{ $formulas->onEachSide(1)->links() }}
+        </div>
     </div>
-</div>
 </div>
