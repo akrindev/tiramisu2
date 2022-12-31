449106b569bfa69a5f48ab553aec0f05cf4a1b0a
akrindev <akrinmin@gmail.com>
2022-12-31 22:51:38 +0700

    fix middleware

--- routes/audit.php
+++ routes/audit.php
@@ -3,4 +3,4 @@
 use App\Http\Livewire\Audit\Index as AuditIndex;
 use Illuminate\Support\Facades\Route;
 
-Route::get('admin/audit', AuditIndex::class)->name('audit');
+Route::get('admin/audit', AuditIndex::class)->middleware(['admin'])->name('audit');
