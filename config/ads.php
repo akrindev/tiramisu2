734e72cb0b8dc44c6699e9fd07902ec230e455ba
Syakirin Amin <33445459+akrindev@users.noreply.github.com>
2024-03-04 23:17:52 +0700

    conf to show ads head

--- config/ads.php
+++ config/ads.php
@@ -2,5 +2,6 @@
 
 return [
     'disable' => env('ADS_DISABLE', false),
+    'show_head' => env('ADS_DISABLE', true),
     'ads_client' => env('ADS_CLIENT', 'ca-pub-1234567890'),
 ];
--- resources/views/inc/ads.blade.php
+++ resources/views/inc/ads.blade.php
@@ -1,4 +1,4 @@
-@if (config('ads.disable') == false || !(new \App\User)->isTopContributor())
+@if (config('ads.show_head') || !(new \App\User)->isTopContributor())
 
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3650356173552443"
      crossorigin="anonymous"></script>
