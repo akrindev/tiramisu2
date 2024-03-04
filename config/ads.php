99e4c13dfad76c1b63bfdbd9801c2a5f42338238
Syakirin Amin <33445459+akrindev@users.noreply.github.com>
2024-03-04 23:19:36 +0700

    fix typo

--- config/ads.php
+++ config/ads.php
@@ -2,6 +2,6 @@
 
 return [
     'disable' => env('ADS_DISABLE', false),
-    'show_head' => env('ADS_DISABLE', true),
+    'show_head' => env('ADS_HEAD', true),
     'ads_client' => env('ADS_CLIENT', 'ca-pub-1234567890'),
 ];
