d0efaf6adb6f8e51a6e13430c27366efb0521fa9
Syakirin Amin <akrinmin@gmail.com>
2024-11-30 20:57:39 +0700

    fix: google one tap

--- routes/web.php
+++ routes/web.php
@@ -87,9 +87,11 @@ Route::get('/fb-login', 'Auth\LoginController@redirect');
 Route::get('/tw-login', 'Auth\LoginController@redirectTwitter');
 Route::get('/google-login', 'Auth\LoginController@redirectGoogle');
 
-Route::match(['get', 'post'], '/facebook/callback', 'Auth\LoginController@callback');
-Route::match(['get', 'post'], '/twitter/callback', 'Auth\LoginController@callbackTwitter');
-Route::match(['get', 'post'], '/google/callback', 'Auth\LoginController@callbackGoogle');
+Route::get('/facebook/callback', 'Auth\LoginController@callback');
+Route::get('/twitter/callback', 'Auth\LoginController@callbackTwitter');
+Route::get('/google/callback', 'Auth\LoginController@callbackGoogle');
+
+Route::post('/google/one-tap', 'Auth\LoginController@handleGoogleOneTap');
 
 Route::get('/profile/{provider_id}', 'UserController@profile');
 
