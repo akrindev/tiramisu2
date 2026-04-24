6576eb3c4714870dd521b8a4a0d892f5091a2477
Syakirin Amin <akrinmin@gmail.com>
2026-04-25 01:48:06 +0700

    Migrate to Laravel 11+ Slim Skeleton (bootstrap/app.php)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,15 @@
 
 namespace App\Providers;
 
+use App\Guild;
+use App\Policies\GuildPolicy;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -29,6 +35,17 @@ class AppServiceProvider extends ServiceProvider
         $locale = Str::contains(request()->getHttpHost(), 'en') ? 'en' : 'id';
 
         App::setLocale($locale);
+
+        $this->configureRateLimiting();
+
+        // Policies
+        Gate::policy(Guild::class, GuildPolicy::class);
+
+        // Events
+        Event::listen(
+            'App\Events\Event',
+            'App\Listeners\EventListener'
+        );
     }
 
     /**
@@ -41,5 +58,17 @@ class AppServiceProvider extends ServiceProvider
         if (app()->isLocal()) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
+    }
+
+    /**
+     * Configure the rate limiters for the application.
+     *
+     * @return void
+     */
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(52)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
--- config/app.php
+++ config/app.php
@@ -157,12 +157,6 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        //App\Providers\TelescopeServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
         App\Providers\CloudinaryServiceProvider::class,
 
     ],
